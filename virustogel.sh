620b89bdfa34605014c59bd037f00f72d6a40193
MrAiGans <79421574+MrAiGans@users.noreply.github.com>
2021-02-27 17:34:45 +0700

    Add files via upload

---
+++ virustogel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+echo -e "###########################################"
+echo -e "###########################################"
+echo -e " Author : MrAiGans"
+echo -e " Tingkat : Newbie Berkarya"
+echo -e " Dari team : DRAGON VIRUS CYBER"
+echo -e "###########################################"
+echo -e "###########################################"
+read -p "Masukan nomer togel ajg : " nomor;
+while [[ true ]]; do
+echo -e "virus masuk ajg ke nomor $nomor"
+done
+
